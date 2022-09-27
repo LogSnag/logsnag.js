b25eeb7b2d8b257e1ed78c888d42a7d721ea265c
martindonadieu <martindonadieu@gmail.com>
2022-09-27 02:41:43 +0200

    fix: publish scripts

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,4 +1,5 @@
 import { build, emptyDir } from "https://deno.land/x/dnt@0.31.0/mod.ts";
+import pkg from "../package.json" assert { type: "json" };
 
 await emptyDir("./npm");
 
@@ -7,46 +8,13 @@ await build({
   outDir: "./npm",
   test: false,
   shims: {
-    // custom: [{
-    //   package: {
-    //     name: "node-fetch",
-    //     version: "~3.1.0",
-    //   },
-    //   globalNames: [{
-    //     // for the `fetch` global...
-    //     name: "fetch",
-    //     // use the default export of node-fetch
-    //     exportName: "default",
-    //   }, {
-    //     name: "RequestInit",
-    //     typeOnly: true, // only used in type declarations
-    //   }],
-    // }, {
-    //   // this is what `blob: true` does internally
-    //   module: "buffer", // uses node's "buffer" module
-    //   globalNames: ["Blob"],
-    // }, {
-    //   // this is what `domException: true` does internally
-    //   package: {
-    //     name: "domexception",
-    //     version: "^4.0.0",
-    //   },
-    //   typesPackage: {
-    //     name: "@types/domexception",
-    //     version: "^4.0.0",
-    //   },
-    //   globalNames: [{
-    //     name: "DOMException",
-    //     exportName: "default",
-    //   }],
-    // }],
     undici: true,
     deno: true,
   },
   package: {
     // package.json properties
     name: "@capgo/logsnag",
-    version: Deno.args[0],
+    version: pkg.version,
     description: "LogSnag Client",
     license: "MIT",
     author: "Shayan",

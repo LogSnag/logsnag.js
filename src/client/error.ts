2946204d3e41444c079496cac0fba798e6716d05
cluzier <connerluzier@protonmail.com>
2023-01-25 22:24:55 -0500

    Add more validation to the constructor to ensure that the arguments passed in are valid

--- src/client/error.ts
+++ src/client/error.ts
@@ -8,6 +8,16 @@ export class HTTPResponseError extends Error {
   public readonly body: any | undefined;
 
   constructor(status: number, statusText: string, body: any | undefined) {
+    // Validate that the status is a positive integer
+    if (typeof status !== 'number' || status < 100 || status >= 600) {
+      throw new Error('Invalid status code');
+    }
+
+    // Validate that the statusText is a non-empty string
+    if (typeof statusText !== 'string' || !statusText) {
+      throw new Error('Invalid status text');
+    }
+
     super(`HTTP Error Response: ${status} ${statusText}`);
     this.status = status;
     this.statusText = statusText;
